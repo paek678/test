477e3c3f077b654aeae16f8b1deac331d7b7fdcb
Seoung Youl <paek678@naver.com>
2024-01-11 17:06:06 +0900

    안녕하세요

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 import java.util.Vector;
 
-public class Main {}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("abce");
+    }
+}
 
